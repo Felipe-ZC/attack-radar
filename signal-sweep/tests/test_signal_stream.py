f94b175bf9dce4cdd4a01a6ad482844d2e0e393e
Felipe Zuniga Carlier <zfelipe@proton.me>
2025-08-18 23:31:49 -0700

    update signal_stream tests

--- signal-sweep/tests/test_signal_stream.py
+++ signal-sweep/tests/test_signal_stream.py
@@ -18,20 +18,37 @@ async def test_write_stream_data(
     sample_stream_data: List[StreamData],
 ):
     signal_stream = SignalStream(mock_redis_client)
+
     for stream_data in sample_stream_data:
         await signal_stream.write_stream_data(stream_data)
         stream_data_as_dict = asdict(stream_data)
+        hash_id = _get_dict_str_hash(stream_data_as_dict)
 
         mock_redis_client.sismember.assert_called_with(
-            DEFAULT_SET_NAME, _get_dict_str_hash(stream_data_as_dict)
+            DEFAULT_SET_NAME, hash_id
         )
 
-        mock_redis_client.sadd.assert_called_with(
-            DEFAULT_SET_NAME, _get_dict_str_hash(stream_data_as_dict)
-        )
+        mock_redis_client.sadd.assert_called_with(DEFAULT_SET_NAME, hash_id)
 
         mock_redis_client.xadd.assert_called_with(
             DEFAULT_STREAM_NAME, stream_data_as_dict
         )
 
-# TODO: test already hashed object
+
+async def test_write_stream_data_dedupe(
+    mock_redis_client: AsyncMock,
+    sample_stream_data: List[StreamData],
+):
+    mock_redis_client.sismember = AsyncMock(return_value=True)
+    signal_stream = SignalStream(mock_redis_client)
+
+    for stream_data in sample_stream_data:
+        await signal_stream.write_stream_data(stream_data)
+        stream_data_as_dict = asdict(stream_data)
+        hash_id = _get_dict_str_hash(stream_data_as_dict)
+
+        mock_redis_client.sismember.assert_called_with(
+            DEFAULT_SET_NAME, hash_id
+        )
+        mock_redis_client.sadd.assert_not_called()
+        mock_redis_client.xadd.assert_not_called()
