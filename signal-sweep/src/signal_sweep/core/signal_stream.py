8f8409e24a22aec6b134732d8cb0761d2059e362
Felipe Zuniga Carlier <zfelipe@proton.me>
2025-08-18 22:29:49 -0700

    WIP: signal_stream unit tests

--- signal-sweep/src/signal_sweep/core/signal_stream.py
+++ signal-sweep/src/signal_sweep/core/signal_stream.py
@@ -11,17 +11,21 @@ DEFAULT_STREAM_NAME = "signal-stream"
 DEFAULT_SET_NAME = "signal-stream-set"
 
 
+def _get_json_str_hash(json_str: str) -> str:
+    return hashlib.sha256(json_str).hexdigest()
+
+
 class SignalStream:
     def __init__(self, redis_client: redis.Redis):
         self.redis_client = redis_client
+        print(self.redis_client)
 
     async def write_stream_data(self, stream_data: StreamData) -> str:
         try:
             data = asdict(stream_data)
-            hash_id = hashlib.sha256(
+            hash_id = _get_json_str_hash(
                 json.dumps(data, sort_keys=True).encode()
-            ).hexdigest()
-
+            )
             if not await self.redis_client.sismember(
                 DEFAULT_SET_NAME, hash_id
             ):
---
+++ signal-sweep/tests/handlers/test_signal_stream.py
@@ -0,0 +1,17 @@
+from typing import List
+from unittest.mock import AsyncMock
+
+from src.signal_sweep.core.signal_stream import SignalStream
+from src.signal_sweep.core.models import StreamData
+
+
+async def test_signal_stream(
+    mock_redis_client: AsyncMock,
+    sample_stream_data: List[StreamData],
+):
+    signal_stream = SignalStream(mock_redis_client)
+    for stream_data in sample_stream_data:
+        await signal_stream.write_stream_data(stream_data)
+        mock_redis_client.sismember.assert_called()
+        mock_redis_client.xadd.assert_called()
+        mock_redis_client.sadd.assert_called()
