7ae9a73fa8786ecc88bd99861a4eec7b2cd84653
Felipe Zuniga Carlier <zfelipe@proton.me>
2026-08-24 19:45:37 -0700

    WIP: collapse all services into a single beacon services that will include the API and data ingestion layer, added support post postgres instead of attack-radar

--- signal-sweep/src/signal_sweep/core/handlers/base_handler.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from radar_core.models import StreamData
-
-
-class Handler(ABC):
-    @abstractmethod
-    def handle(self, url: str) -> list[StreamData]:
-        pass
--- signal-sweep/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-# Test package for signal_sweep
