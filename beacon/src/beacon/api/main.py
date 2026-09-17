22423045bb67c842521852eaae000c6bd42945bc
Felipe Zuniga Carlier <zfelipe@proton.me>
2026-09-16 21:22:49 -0700

    fixed config bug, cleaned up main

--- beacon/src/beacon/api/main.py
+++ beacon/src/beacon/api/main.py
@@ -1,50 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import APIRouter, FastAPI, Request, status
 
+from beacon.data.db import DBClient
+from beacon.shared.config import settings
+
+
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
-    logger = setup_logger(log_level=get_log_level_from_env())
-    app.state.logger = logger
-    logger.info("Starting up (env=%s)", settings.APP_ENV)
-
-    app.state.slack = SlackService(
-        settings.SLACK_BOT_TOKEN,
-        settings.SLACK_CHANNEL,
-        settings.SLACK_API_BASE_URL,
-        logger,
+async def lifespan(app: FastAPI):
+    postgres = DBClient(
+        host=settings.POSTGRES_HOST,
+        port=settings.POSTGRES_PORT,
+        database=settings.POSTGRES_DB,
+        user=settings.POSTGRES_USER,
+        password=settings.POSTGRES_PASSWORD,
     )
-    await app.state.slack.connect()
-
-    app.state.traceability = TraceabilityService(
-        settings.TRACEABILITY_API_BASE_URL,
-        settings.TRACEABILITY_TIMEOUT_SECONDS,
-        logger,
-    )
-    await app.state.traceability.connect()
-
-    app.state.qrmac_db = AsyncSQLServerClient(
-        settings.INT_QRMAC_DB_DSN, settings.DB_POOL_SIZE, logger
-    )
-    app.state.trace_db = AsyncSQLServerClient(
-        settings.TRACE_DB_DSN, settings.DB_POOL_SIZE, logger
-    )
-    # app.state.shipping_db = AsyncSQLServerClient(
-    #     settings.SHIPPING_DB_DSN, settings.DB_POOL_SIZE, logger
-    # )
-    await app.state.qrmac_db.connect()
-    await app.state.trace_db.connect()
-    # await app.state.shipping_db.connect()
-
-    app.state.product_config_list = await _load_product_configs(
-        app.state.trace_db, logger
-    )
-
+    await postgres.connect()
+    app.state.db = postgres
     yield
-    logger.info("Shutting down")
-    await app.state.qrmac_db.disconnect()
-    await app.state.trace_db.disconnect()
-    # await app.state.shipping_db.disconnect()
-    await app.state.slack.disconnect()
-    await app.state.traceability.disconnect()
+    await postgres.disconnect()
 
 
 app = FastAPI(lifespan=lifespan)
