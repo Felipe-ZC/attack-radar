a9d61c831c3298ffac173536e38ede35a38b40e1
Felipe ZC <fzunigacar@gmail.com>
2026-09-21 18:05:54 -0700

    Restore init db file

--- docker/initdb/01-schema.sql
+++ docker/initdb/01-schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS public.host_metadata
 (
-    ip_address inet PRIMARY KEY,
+    ip_address inet NOT NULL,
     country_code text COLLATE pg_catalog."default",
     country_name text COLLATE pg_catalog."default",
     usage_type text COLLATE pg_catalog."default",
     domain text COLLATE pg_catalog."default",
     isp text COLLATE pg_catalog."default",
     lat double precision,
-    lon double precision
+    lon double precision,
+    CONSTRAINT host_metadata_ip_address_key UNIQUE (ip_address)
 );
 
 CREATE TABLE IF NOT EXISTS abuse_ipdb_reports (
